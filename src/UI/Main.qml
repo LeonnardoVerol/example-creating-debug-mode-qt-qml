7f26269ab0e5a3a8063634d1f6086cb9755fdfcd
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2021-09-26 11:14:41 -0300

    Add Example

--- src/UI/Main.qml
+++ src/UI/Main.qml
@@ -5,5 +5,13 @@ Window {
     width: 640
     height: 480
     visible: true
-    title: qsTr("Hello World")
+    title: qsTr("Example Creating Debug Mode in QT/QML")
+    color: QT_DEBUG ? "yellow" : "green"
+
+    Text {
+        id: name
+        anchors.centerIn: parent
+        text: QT_DEBUG ? "Debug Mode" : "Release Mode"
+        font.pixelSize: 28
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext> // Add
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/UI/Main.qml"));
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+
     engine.load(url);
+
+    // Add
+#ifdef QT_DEBUG
+    engine.rootContext()->setContextProperty("QT_DEBUG", true);
+#else
+    engine.rootContext()->setContextProperty("QT_DEBUG", false);
+#endif
 
     return app.exec();
 }
